3b7e07b80b73d9b610ba50f29107bf05c002d195
TommasoGalistu <tommasogalistu92@gmail.com>
2024-05-29 13:40:51 +0200

    aggiunta funzionalità finita add student

--- bonus/js.js
+++ bonus/js.js
@@ -1,5 +1,21 @@
+// pulsante +, per aggiunta studente
 let pulsanteAggStudente = document.querySelector('.pulsanteAggiungiStudente')
+// slidebar laterale da aprire
+let slideMenulaterale = document.querySelector('.slideMenu')
+// x per chiudere
+let xClose = document.querySelector('.chiudiFinestra')
+// background della slidebar laterale
+let backgroundSlideBar = document.querySelector('.backGroundProva')
+
 
 pulsanteAggStudente.addEventListener('click', () =>{
-    console.log('dentro')
+
+    slideMenulaterale.classList.remove('d-none')
+    backgroundSlideBar.classList.remove('d-none')
+
+})
+
+xClose.addEventListener('click', () => {
+    slideMenulaterale.classList.add('d-none')
+    backgroundSlideBar.classList.add('d-none')
 })
